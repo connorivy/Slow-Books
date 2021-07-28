ed0fff4e7518aee0ab1401c857d8d80895c9ff35
connorivy <43247197+connorivy@users.noreply.github.com>
2021-07-28 12:26:18 -0500

    Now tracking dist and split bs page in base.html and bs.html

--- balancesheet/views.py
+++ balancesheet/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'balancesheet/base.html')
+    return render(request,'balancesheet/balancesheet.html')
