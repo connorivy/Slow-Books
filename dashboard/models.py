8b34edc6751e8cfdebb55f7f40312d6718ef0ed2
connorivy <connorivy15@utexas.edu>
2021-07-28 23:30:24 -0500

    creating first db model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class employees(models.Model):
+    fname = models.CharField(max_length=25)
+    lname = models.CharField(max_length=25)
+    add1 = models.CharField(max_length=25)
+    add2 = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=25)
+    zipcode = models.IntegerField()
+    ssn = models.CharField(max_length=25)
+    withholding = models.IntegerField(max_length=25)
+    salary = models.CharField(max_length=25)
+    message = fname = models.CharField(max_length=100)
+
